1a9a96530a22fe30cff8046505639384ae24d479
davidhardin <thisgoestodavid@gmail.com>
2025-07-18 16:29:06 +0300

    test mongo

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,18 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-export type CatDocument = HydratedDocument<Cat>;
+export type UserDocument = HydratedDocument<User>;
 
 @Schema()
-export class Cat {
+export class User {
   @Prop()
   name: string;
-
-  @Prop()
-  age: number;
-
-  @Prop()
-  breed: string;
 }
 
-export const CatSchema = SchemaFactory.createForClass(Cat);
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const savedUser = new this.userModel({ name: 'Frodo Baggings' });
+    return savedUser.save();
+    // return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find().exec();
+  }
+  returnHey() {
+    return 'Hey!';
   }
 
   findOne(id: number) {
